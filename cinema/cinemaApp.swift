ad53145d7cbfe5eb2e0d48a82b084738ec37e990
danilacrazych <danilacrazych@mail.ru>
2024-08-12 15:20:31 +0300

    Initial Commit

---
+++ cinema/cinemaApp.swift
@@ -0,0 +1,17 @@
+//
+//  cinemaApp.swift
+//  cinema
+//
+//  Created by Данил Уваров on 12.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct cinemaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
