fc82971e9eff75e4b6aaefe091bb8a103dff0629
danilacrazych <danilacrazych@mail.ru>
2024-08-12 17:18:09 +0300

    First ios app

---
+++ cinema/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  cinema
+//
+//  Created by Данил Уваров on 12.08.2024.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
